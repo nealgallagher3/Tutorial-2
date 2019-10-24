162c5a89eeed8a2bb74eeb28ebe307dbb42ac68b
nealgallagher3 <54585470+nealgallagher3@users.noreply.github.com>
2019-10-24 17:58:12 -0400

    Challenge 2

--- Tutorial 2/Assets/Scripts/PlayerScript.cs
+++ Tutorial 2/Assets/Scripts/PlayerScript.cs
@@ -6,18 +6,29 @@ using UnityEngine.UI;
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
-
     public float speed;
-
     public Text score;
-
     private int scoreValue = 0;
+    public Text winText;
+    public Text livesText;
+    private int lives = 3;
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    Animator anim;
+    private bool facingRight = true;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        SetScoreText();
+        scoreValue = 0;
+        winText.text = "";
+        livesText.text = "Lives: " + lives.ToString();
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,10 +37,37 @@ public class PlayerScript : MonoBehaviour
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+        /*
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetBool("Jumping",true);
+        }
+        */
+        if (vertMovement != 0)
+        {
+            anim.SetBool("Jumping", true);
+        }
         if (Input.GetKey("escape"))
         {
             Application.Quit();
         }
+        if (hozMovement != 0)
+        {
+            anim.SetInteger("State", 1);
+        }
+        else
+        {
+            anim.SetInteger("State", 0);
+        }
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -37,8 +75,20 @@ public class PlayerScript : MonoBehaviour
         if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            SetScoreText();
             Destroy(collision.collider.gameObject);
+            
+        }
+        if (collision.collider.tag == "UFO")
+        {
+            lives = lives - 1;
+            livesText.text = "Lives: " + lives.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+        if (lives == 0)
+        {
+            Destroy(this);
+            winText.text = "You Lose!";
         }
 
     }
@@ -51,6 +101,31 @@ public class PlayerScript : MonoBehaviour
             {
                 rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
             }
+            anim.SetBool("Jumping", false);
         }
+    }
+    void SetScoreText()
+    {
+        score.text = "Score: " + scoreValue.ToString();
+        if (scoreValue == 4)
+        {
+            transform.position = new Vector2(50.0f, 0.0f);
+            lives = 3;
+            livesText.text = "Lives: " + lives.ToString();
+        }
+        if (scoreValue == 8)
+        {
+            winText.text = "You win! Game created by Neal Gallagher!";
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+        }
+        
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
     }
 }
